05deea21af302a201c974c23dc3a0fba7b94fe11
harry101491 <hp1014@nyu.edu>
2022-02-01 13:45:39 -0800

    task(depth first in graph)

---
+++ depthFirstSearch.js
@@ -0,0 +1,31 @@
+const graph = {
+    a: ['c', 'b'],
+    b: ['d'],
+    c: ['e'],
+    d: ['f'],
+    e: [],
+    f: []
+};
+
+const depthFirstPrint = (graph, start) => {
+    const stack = [ start ];
+
+    while (stack.length > 0) {
+        const current = stack.pop();
+        console.log(current);
+
+        for (const adjacent of graph[current]) {
+            stack.push(adjacent);
+        }
+    }
+}
+
+const depthFirstPrintRecurse = (graph, start) => {
+    console.log(start);
+    for (const adjacent of graph[start]) {
+        depthFirstPrintRecurse(graph, adjacent);
+    }
+}
+
+depthFirstPrint(graph, "a");
+depthFirstPrintRecurse(graph, "a");
