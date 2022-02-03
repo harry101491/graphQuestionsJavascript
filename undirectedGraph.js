2025af9a03ac804ff8b5b3de4857236a1049d309
harry101491 <hp1014@nyu.edu>
2022-02-02 16:23:21 -0800

    task(has path undirected graph)

--- undirectedGraph.js
+++ undirectedGraph.js
@@ -30,4 +30,29 @@ const populatedUndirectedGraph = (edges) => {
     return graph;
 }
 
-console.log(populatedUndirectedGraph(edges));
+const hasPathUndirectedGraph = (graph, src, dest, visited) => {
+    if (visited.has(src)) return false;
+    if (src === dest) return true;
+    
+    visited.add(src);
+
+    for (const adjacent of graph[src]) {
+        if (hasPathUndirectedGraph(graph, adjacent, dest, visited)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+const isPathPresentFromSrcToDest = (edges, src, dest) => {
+    const graph = populatedUndirectedGraph(edges);
+
+    if (hasPathUndirectedGraph(graph, src, dest, new Set())) {
+        console.log("has a path");
+    } else {
+        console.log("has no path");
+    }
+}
+
+isPathPresentFromSrcToDest(edges, "j", "m");
