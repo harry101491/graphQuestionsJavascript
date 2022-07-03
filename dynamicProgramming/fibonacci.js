8e2c2940f93b4374aba75ce6ddae110fe0a93013
harry101491 <hp1014@nyu.edu>
2022-07-03 13:57:08 -0700

    feat(adding readme for dynamic programming)

--- dynamicProgramming/fibonacci.js
+++ dynamicProgramming/fibonacci.js
@@ -1,6 +1,6 @@
 // with memoization
 const fibWithMemo = (n, memo = {}) => {
-  if (n <= 2) memo[n] = 1;
+  if (n <= 2) return 1;
   if (n in memo) return memo[n];
   memo[n] = fibWithMemo(n - 1, memo) + fibWithMemo(n - 2, memo);
   return memo[n];
